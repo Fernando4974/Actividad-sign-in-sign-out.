30ad573e9f3d9da48fa06a1af882af2308b41e59
Yesid <yesidusugalondono@gmail.com>
2025-11-24 14:04:40 -0500

    AAdded: pokemonScreen and PokemonItem, tried to run but keeps loading

--- app/src/main/java/com/sena/practica_de_consumo_api/App.kt
+++ app/src/main/java/com/sena/practica_de_consumo_api/App.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.sena.practica_de_consumo_api.presentation.user.PokemonScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -18,14 +19,16 @@ fun App(){
             )
         }
     ) { innerPadding ->
-        UserScreen(
+        PokemonScreen(
             modifier = Modifier.padding(innerPadding)
         )
     }
 }
 
+/**
 @Composable
 fun UserScreen(modifier: Modifier = Modifier) {
     Text(modifier = modifier,
         text = "Hola")
 }
+        **/
--- app/src/main/java/com/sena/practica_de_consumo_api/presentation/user/pokemonScreen.kt
+++ app/src/main/java/com/sena/practica_de_consumo_api/presentation/user/pokemonScreen.kt
@@ -1,2 +1,53 @@
 package com.sena.practica_de_consumo_api.presentation.user
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.sena.practica_de_consumo_api.presentation.user.components.PokemonItem
+
+
+@Composable
+fun PokemonScreen(
+    modifier: Modifier = Modifier,
+    viewModel: PokemonViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        if (uiState.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+
+        uiState.errorMessage?.let { error ->
+            Text(
+                text = error,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+
+        if (!uiState.isLoading && uiState.errorMessage == null) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(uiState.pokemon) { pokemon ->
+                    PokemonItem(pokemon = pokemon)
+                }
+            }
+        }
+    }
+}
